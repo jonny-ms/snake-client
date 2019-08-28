0825219fc3ff293e8a62ff47f8bd79ad93d5888a
Jonathan <j.milne21@gmail.com>
2019-08-28 17:14:15 +0000

    cleaned up by separating modules as input, connect, play

--- play.js
+++ play.js
@@ -1,22 +1,7 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
+
 console.log('Connecting ...');
 connect();
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput)
-  return stdin;
-}
-
-const handleUserInput = (data) => {
-  if (data === '\u0003') process.exit();
-}
-
-setupInput()
+setupInput();

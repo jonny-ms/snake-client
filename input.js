00291a381428e049a0c901385c7103e9ed1623a5
Jonathan <j.milne21@gmail.com>
2019-08-28 17:47:37 +0000

    the snake moves!

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -9,6 +12,10 @@ const setupInput = function() {
 
 const handleUserInput = (data) => {
   if (data === '\u0003') process.exit();
+  if (data === "\u0077") connection.write("Move: up");
+  if (data === "\u0073") connection.write("Move: down");
+  if (data === "\u0061") connection.write("Move: left");
+  if (data === "\u0064") connection.write("Move: right");
 }
 
 module.exports = { setupInput } 
